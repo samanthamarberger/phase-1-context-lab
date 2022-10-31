d0ca83e3176bb8a1ab99e2ffac3e1d0f3698d0f5
samanthamarberger <slatham04@gmail.com>
2022-10-31 11:29:05 -0600

    commit tree 20a5f0f8d06ebf82e4d370beb03aba21916cfae0

--- index.js
+++ index.js
@@ -21,3 +21,89 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employeeArray) {
+    let employeeObj = {
+        firstName:employeeArray[0],
+        familyName:employeeArray[1],
+        title:employeeArray[2],
+        payPerHour:employeeArray[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+        }
+        return employeeObj;
+}
+
+function createEmployeeRecords(arrayOfEmployees) {
+    let employeesArray = [];
+    for(let i = 0; i < arrayOfEmployees.length; i++){
+        let employeeObj = createEmployeeRecord(arrayOfEmployees[i]);
+        employeesArray.push(employeeObj);
+    }
+    return employeesArray;
+}
+
+function createTimeInEvent(timeIn) {
+    let timeInArray = timeIn.split(" ");
+    let timeInObj = {};
+    timeInObj.type = "TimeIn";
+    timeInObj.date = timeInArray[0];
+    timeInObj.hour = Number(timeInArray[1]);
+    this.timeInEvents.push(timeInObj);
+    return this;
+}
+
+function createTimeOutEvent(timeOut){
+    let timeOutArray = timeOut.split(" ");
+    let timeOutObj = {};
+    timeOutObj.type = "TimeOut";
+    timeOutObj.date = timeOutArray[0];
+    timeOutObj.hour = Number(timeOutArray[1]);
+    this.timeOutEvents.push(timeOutObj);
+    return this;
+}
+
+function hoursWorkedOnDate(dateIn){
+    console.log(`date:`, dateIn);
+    let clockIn = -1;
+    let clockOut = -1;
+    //console.log("object:", this.timeInEvents);
+    for(const eventIn of this.timeInEvents){
+        //console.log("eventIn:" , eventIn);
+        if (eventIn.date === dateIn) {
+            clockIn = eventIn.hour;
+        }
+    }
+    for(const eventOut of this.timeOutEvents){
+        //console.log("eventOut", eventOut);
+        if (eventOut.date === dateIn) {
+            clockOut = eventOut.hour;
+        } 
+    }
+    return Math.abs(clockIn - clockOut)/100;
+}
+
+function wagesEarnedOnDate(dateIn){
+    let hours = hoursWorkedOnDate.call(this, dateIn);
+    //console.log("hours:", hours);
+    let wage = this.payPerHour;
+    return hours * wage;
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    for(const employee of srcArray){
+        if(employee.firstName === firstName){
+            return employee;
+        }
+    }
+}
+
+function calculatePayroll(employeeRecords){
+    let payrollTotal = 0;
+    for(const employee of employeeRecords){
+        console.log("employee:", employee);
+        let individualPay = allWagesFor.call(employee);
+        payrollTotal = payrollTotal + individualPay;
+    }
+    return payrollTotal;
+}
+
